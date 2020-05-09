9e0dbc5fb385e1915dcb733d38183885c2547244
Davide <ddesimone1@gmail.com>
2020-05-09 19:12:07 -0400

    changed balloon box so it doesn't need to rotate on gravity change

---
+++ src/Objects/BalloonBoxBody.gd
@@ -0,0 +1,14 @@
+extends RigidBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func orient_to_gravity(angle):
+	get_parent().orient_to_gravity(angle)
--- src/Objects/BoxWithBalloons.gd
+++ src/Objects/BoxWithBalloons.gd
@@ -10,7 +10,9 @@ func _ready():
 	$GravityController.set_gravity_direction(-gravity_vector)
 	
 func orient_to_gravity(angle):
+	set_mode(RigidBody2D.MODE_KINEMATIC)
 	rotate(-angle)
+	set_mode(RigidBody2D.MODE_CHARACTER)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
